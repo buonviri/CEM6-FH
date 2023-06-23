5c4c455401638da0820cfff09dff350fc2526921
buonviri <andy@buonviri.com>
2023-06-22 22:02:17 -0400

    added 'both' text as a test

--- CEM6-FH.py
+++ CEM6-FH.py
@@ -1,7 +1,7 @@
 
 fn = 'C:/EdgeCortix/CEM6/CEM6-FH.FCStd'  # set filename
 
-panel_x = 7.50                           # x dimension of PCIe bracket mounting holes
+panel_x_both = 7.50                      # x dimension of both PCIe bracket mounting holes
 panel_y_lower = 9.35                     # y dimension of lower PCIe bracket mounting hole
 panel_y_upper = 9.35 + 85.4              # y dimension of upper PCIe bracket mounting hole
 panel_tab_width = 7.95                   # width of PCIe bracket mounting tab
@@ -22,7 +22,7 @@ pwb.Label = "PWB"
 s = panel_y_lower - panel_tab_width/2
 n = panel_y_upper + panel_tab_width/2
 w = 0.0  # aligned with card edge
-e = panel_x + panel_tab_length/2
+e = panel_x_both + panel_tab_length/2
 tabs = doc.addObject("Sketcher::SketchObject", "Sketch")  # create sketch for tabs boundary
 tabs.addGeometry(Part.LineSegment(App.Vector(w, s, 0), App.Vector(e, s, 0)), False)
 tabs.addGeometry(Part.LineSegment(App.Vector(e, s, 0), App.Vector(e, n, 0)), False)
@@ -46,3 +46,5 @@ doc.recompute()  # redraw
 Gui.SendMsgToActiveView("ViewFit")  # fit all
 
 doc.saveAs(fn)
+
+# end of CEM6-FH
